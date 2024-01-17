835152befdaca0f17f6f4cd3eb94cdb46d73c1ba
Girish <gspande7@gmail.com>
2024-01-16 20:32:46 -0500

    WIP

---
+++ express-fortune/main.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+
+const app = express();
+const config = { port: process.env.PORT || 3000 }
+const cors = require ('cors')
+app.get("/api/fortune", (req, res) => {
+  const fortune = 
+  [
+    "Embrace the learning journey. Coding experience is built step by step.",
+    "Your dedication to learning to code will open doors to new opportunities.",
+    "Just like debugging, challenges are temporary. Keep coding!",
+    "In the world of tech, every line of code you write is a step toward success.",
+    "Your career switch into tech is like a well-optimized algorithm: efficient and rewarding.",
+    "Every error is an opportunity to learn something new. Don't give up!"
+  ];
+  res.json({})
+})
