269e8686f23c41e28279073dd37e5e97807d45bb
courtneysembly <courtneysembly@gmail.com>
2024-01-18 19:59:54 -0500

    Fortune

--- express-fortune/main.js
+++ express-fortune/main.js
@@ -1,11 +1,7 @@
 const express = require("express");
 
 const app = express();
-<<<<<<< HEAD
-const config = { port: process.env.PORT || 3000 }
-=======
 const config = { port: process.env.PORT || 3000 };
->>>>>>> ba83756363379363460280608ce1bffa83bb7acb
 app.get("/api/fortune", (req, res) => {
   const fortune = [
     "Embrace the learning journey. Coding experience is built step by step.",
@@ -15,16 +11,10 @@ app.get("/api/fortune", (req, res) => {
     "Your career switch into tech is like a well-optimized algorithm: efficient and rewarding.",
     "Every error is an opportunity to learn something new. Don't give up!",
   ];
-
   const randomFortune = fortune[Math.floor(Math.random() * fortune.length)];
-
   res.json({ Fortune: randomFortune });
-<<<<<<< HEAD
-  app.listen(config.port, () => {
+} ) 
+ app.listen(config.port, () => {
 
     console.log('App listening on http://localhost:${config.port}');
-  } ) 
-  })
-=======
-});
->>>>>>> ba83756363379363460280608ce1bffa83bb7acb
+  });
